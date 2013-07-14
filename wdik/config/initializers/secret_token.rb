96f95e5ee626a6751797d220692c69e439335e6b
Patsy Price <patsy.m.price@gmail.com>
2013-07-14 11:35:28 -0700

    initial commit

---
+++ wdik/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wdik::Application.config.secret_token = '396d8a244743ab8475094df26a592bf95e51c9494402fae166bd58eb6c8ea203d72e6e752ee5b1cb4ac77034b4d8ed89ec756d08a216a13f55e3fb8ab514ce78'
